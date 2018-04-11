5d758846078a103a4c318c7e1dbfa678251c9d93
DruidKuma <druidkuma@gmail.com>
2018-04-12 02:03:06 +0300

    Tuned PID gains

--- src/PID.cpp
+++ src/PID.cpp
@@ -2,20 +2,30 @@
 
 using namespace std;
 
-/*
-* TODO: Complete the PID class.
-*/
-
 PID::PID() {}
 
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+
+    // init gains
+	this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
+
+    // init errors
+    i_error = 0.0;
+    d_error = 0.0;
+    p_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+    d_error = cte - p_error;
+    i_error = i_error + cte;
+    p_error = cte;
 }
 
 double PID::TotalError() {
+	return -(Kp * p_error + Ki * i_error + Kd * d_error);
 }
 
